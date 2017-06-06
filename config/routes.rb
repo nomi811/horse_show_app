bb4d85eba357560ff277f6fe026a55cb5f0bf251
Nomi Vos <nvoswork@gmail.com>
2017-06-06 16:15:31 -0500

    sets up messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
 end
