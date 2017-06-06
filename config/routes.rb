02b2fc46502a3cd74c741b988397eaeb7497a27b
Nomi Vos <nvoswork@gmail.com>
2017-06-06 18:53:06 -0500

    styling for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
-  get 'static_pages/contact'
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
 end
